2aa0c5dc6faf6cc188c3eb067ad62048b2e844fe
shailesz <shailesz@duck.com>
2022-11-20 11:43:56 +0545

    init commit

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/contrib/static"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	// gin default router
+	router := gin.Default()
+
+	// serve frontend static files
+	router.Use(static.Serve("/", static.LocalFile("./views", true)))
+
+	// setup route group for API
+	api := router.Group("/api")
+
+	api.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	// Start and run the server
+	router.Run(":3000")
+}
